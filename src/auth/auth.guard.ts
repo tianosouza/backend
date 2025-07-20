f43522da444fd98aada38eae8226df565a8dace2
Cristiano Souza <ltianosouzal@outlook.com>
2025-07-20 08:20:16 -0300

    implements AuthGuard

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,41 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('No token provided');
+    }
+
+    try {
+      const payload = this.jwtService.verifyAsync<{
+        name: string;
+        email: string;
+      }>(token, {
+        algorithms: ['HS256'],
+      });
+
+      return true;
+    } catch (error) {
+      console.error('Token verification failed:', error);
+      throw new UnauthorizedException('Invalid token', { cause: error });
+    }
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private prismaService: PrismaService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: loginDto.email },
+    });
+
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+
+    const isPasswordValid = bcrypt.compareSync(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new Error('Invalid credentials');
+    }
+
+    const token = this.jwtService.sign({ name: user.name, email: user.email });
+    return { accessToken: token };
+  }
+}
