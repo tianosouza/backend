79aa0b3d94cdbf4e71bc1c48f23bff6765c6df4f
Cristiano Souza <130114165+tianosouza@users.noreply.github.com>
2025-07-22 16:25:20 -0300

    Updated package-lock.json

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
